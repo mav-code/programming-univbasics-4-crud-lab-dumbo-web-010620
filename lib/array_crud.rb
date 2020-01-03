04659bded532fdb847f38705358e00a73be2b93e
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 09:02:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,9 +19,9 @@ def remove_element_from_end_of_array(array)
  array.shift
  end
 
-# def retrieve_element_from_index(array, index_number)
-#   array[index_number]
-# end
+ def retrieve_element_from_index(array, index_number)
+  array[index_number]
+end
 
 # def retrieve_first_element_from_array(array)
   # array[0]
