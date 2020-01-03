27a68c204adbeb69021a5b50d6fba6178fe048cd
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 09:01:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 
-# def add_element_to_start_of_array(array, element)
- # array.unshift(element)
-# end
+ def add_element_to_start_of_array(array, element)
+  array.unshift(element)
+ end
 
 # def remove_element_from_end_of_array(array)
 # end
