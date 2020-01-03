eef87c5c8d78f4fa4964b3233eaaf49f138c11ce
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 09:04:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,6 +6,9 @@ def create_an_array
   [1, 2, 3, 4]
 end
 
+def add_element_to_end_of_array(array, element)
+ array << element
+end
 
  def add_element_to_start_of_array(array, element)
   array.unshift(element)
