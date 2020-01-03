05bad19ea4aabc3720086fc4cebac5856b41e072
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 09:02:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,6 +31,6 @@ end
    array[-1]
  end
 
- def update_element_from_index(array, index_number, element)
- array[index_number](element)
- end
+ # def update_element_from_index(array, index_number, element)
+ # array[index_number](element)
+ # end
